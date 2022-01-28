1ebf048e4e87975b0f50050bc6957ea792839d84
minenhle <minenhle@skadush.co.za>
2022-01-28 10:08:41 +0200

    - fortnite; + congito role

--- completion/identity.bash
+++ completion/identity.bash
@@ -1,7 +1,5 @@
 #https://opensource.com/article/18/3/creating-bash-completion-script
 # add the following options to login script
-#complete -W "now tomorrow never" identity
-
 completions()
 {
   if [ "$COMP_CWORD" == "1" ]; then
@@ -9,7 +7,7 @@ completions()
     COMPREPLY=($(compgen -W "lipa skadush" "${COMP_WORDS[COMP_CWORD]}"))
   elif [ "$COMP_CWORD" == "2" ]; then
     if [ "${COMP_WORDS[1]}" == "lipa" ]; then
-      COMPREPLY=($(compgen -W "eks-dev-env-developer eks-dev-env-operator eks-dev-env-cluster-operator" "${COMP_WORDS[COMP_CWORD]}"))
+      COMPREPLY=($(compgen -W "eks-dev-env-developer eks-dev-env-operator eks-dev-env-cluster-operator cognito-tenants-dev-env-operator" "${COMP_WORDS[COMP_CWORD]}"))
     elif [  "${COMP_WORDS[1]}" == "skadush"  ]; then
       COMPREPLY=($(compgen -W "minenhle" "${COMP_WORDS[COMP_CWORD]}"))
     fi
